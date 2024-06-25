3234e82074c6a8a2c9658f8ea06e6e2ee725dd66
Joel Christner <joel.christner@gmail.com>
2024-06-25 10:36:18 -0700

    NuGet v0.3.0.15, content type detector SDK

---
+++ src/Test.TypeDetector/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace Test.TypeDetector
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+    using GetSomeInput;
+    using View.Sdk.TypeDetector;
+    using View.Sdk.Shared.Udr;
+    using View.Serializer;
+
+    public static class Program
+    {
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+
+        private static bool _RunForever = true;
+        private static string _Endpoint = "http://localhost:8501/processor/typedetector";
+        private static ViewTypeDetectorSdk _Sdk = null;
+        private static SerializationHelper _Serializer = new SerializationHelper();
+        private static bool _EnableLogging = true;
+
+        public static void Main(string[] args)
+        {
+            _Endpoint = Inputty.GetString("Endpoint :", _Endpoint, false);
+
+            _Sdk = new ViewTypeDetectorSdk(_Endpoint);
+            if (_EnableLogging) _Sdk.Logger = Console.WriteLine;
+
+            while (_RunForever)
+            {
+                string userInput = Inputty.GetString("Command [?/help]:", null, false);
+
+                switch (userInput)
+                {
+                    case "q":
+                        _RunForever = false;
+                        break;
+                    case "?":
+                        Menu();
+                        break;
+                    case "cls":
+                        Console.Clear();
+                        break;
+
+                    case "conn":
+                        TestConnectivity().Wait();
+                        break;
+
+                    case "process":
+                        Process().Wait();
+                        break;
+                }
+            }
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Available commands:");
+            Console.WriteLine("  q             Quit this program");
+            Console.WriteLine("  ?             Help, this menu");
+            Console.WriteLine("  cls           Clear the screen");
+            Console.WriteLine("  conn          Test connectivity");
+            Console.WriteLine("");
+            Console.WriteLine("  process       Process a type detection request");
+            Console.WriteLine("");
+        }
+
+        private static void EnumerateResponse(object obj)
+        {
+            Console.WriteLine("");
+            Console.Write("Response:");
+            if (obj != null)
+                Console.WriteLine(Environment.NewLine + _Serializer.SerializeJson(obj, true));
+            else
+                Console.WriteLine("(null)");
+            Console.WriteLine("");
+        }
+
+        private static async Task TestConnectivity()
+        {
+            Console.WriteLine("");
+            Console.Write("State: ");
+
+            if (await _Sdk.ValidateConnectivity())
+                Console.WriteLine("Connected");
+            else
+                Console.WriteLine("Not connected");
+
+            Console.WriteLine("");
+        }
+
+        private static async Task Process()
+        {
+            string file = Inputty.GetString("Filename:", null, true);
+            
+            if (!String.IsNullOrEmpty(file))
+            {
+                string contentType = Inputty.GetString("Content type:", null, true);
+
+                TypeResult tr = await _Sdk.Process(File.ReadAllBytes(file), contentType);
+                EnumerateResponse(tr);
+            }
+        }
+
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+    }
+}
