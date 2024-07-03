a5c57539a745dd6e4b20e561d047d9179b5f3b36
Joel Christner <joel.christner@gmail.com>
2024-07-02 21:59:23 -0700

    NuGet v0.4.0.6, TimeHelper

---
+++ src/View.Sdk/Helpers/TimeHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace View.Sdk.Helpers
+{
+    using System;
+
+    /// <summary>
+    /// Time helper.
+    /// </summary>
+    public static class TimeHelper
+    {
+        /// <summary>
+        /// Determine the total number of milliseconds between a start and end time.
+        /// </summary>
+        /// <param name="start">Start time.</param>
+        /// <param name="end">End time.</param>
+        /// <returns>Milliseconds.</returns>
+        public static double TotalMsBetween(DateTime start, DateTime end)
+        {
+            try
+            {
+                start = start.ToUniversalTime();
+                end = end.ToUniversalTime();
+                TimeSpan total = end - start;
+                return total.TotalMilliseconds;
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+    }
+}
