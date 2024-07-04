e1ea0d800afb8b04566aeb0dbc6c8db58628e06e
Joel Christner <joel.christner@gmail.com>
2024-07-03 21:45:55 -0700

    Nuget v0.4.0.14, EnumerationOrderEnum

---
+++ src/View.Sdk/EnumerationOrderEnum.cs
@@ -0,0 +1,43 @@
+﻿namespace View.Sdk
+{
+    using System.Runtime.Serialization;
+    using System.Text.Json.Serialization;
+
+    /// <summary>
+    /// Enumeration order.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum EnumerationOrderEnum
+    {
+        /// <summary>
+        /// CreatedAscending.
+        /// </summary>
+        [EnumMember(Value = "CreatedAscending")]
+        CreatedAscending,
+        /// <summary>
+        /// CreatedDescending.
+        /// </summary>
+        [EnumMember(Value = "CreatedDescending")]
+        CreatedDescending,
+        /// <summary>
+        /// LastAccessAscending.
+        /// </summary>
+        [EnumMember(Value = "LastAccessAscending")]
+        LastAccessAscending,
+        /// <summary>
+        /// LastAccessDescending.
+        /// </summary>
+        [EnumMember(Value = "LastAccessDescending")]
+        LastAccessDescending,
+        /// <summary>
+        /// KeyAscending.
+        /// </summary>
+        [EnumMember(Value = "KeyAscending")]
+        KeyAscending,
+        /// <summary>
+        /// KeyDescending.
+        /// </summary>
+        [EnumMember(Value = "KeyDescending")]
+        KeyDescending
+    }
+}
