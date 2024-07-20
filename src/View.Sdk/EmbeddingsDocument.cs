cd182da2249566a21b5bdf9626b4e1e454744e56
Joel Christner <joel.christner@gmail.com>
2024-07-19 21:35:20 -0700

    NuGet v0.4.0.40, embeddings document change

--- src/View.Sdk/EmbeddingsDocument.cs
+++ src/View.Sdk/EmbeddingsDocument.cs
@@ -88,6 +88,11 @@
         public string ObjectVersion { get; set; } = null;
 
         /// <summary>
+        /// Model.
+        /// </summary>
+        public string Model { get; set; } = null;
+
+        /// <summary>
         /// Embeddings rule.
         /// </summary>
         public EmbeddingsRule EmbeddingsRule { get; set; } = null;
@@ -138,10 +143,16 @@
             {
                 Id = Convert.ToInt32(row["id"]),
                 TenantGUID = row["tenant_guid"] != null ? row["tenant_guid"].ToString() : null,
+                CollectionGUID = row["collection_guid"] != null ? row["collection_guid"].ToString() : null,
+                SourceDocumentGUID = row["source_document_guid"] != null ? row["source_document_guid"].ToString() : null,
+                VectorRepositoryGUID = row["vector_repository_guid"] != null ? row["vector_repository_guid"].ToString() : null,
+                GraphRepositoryGUID = row["graph_repository_guid"] != null ? row["graph_repository_guid"].ToString() : null,
+                GraphNodeIdentifier = row["graph_node_identifier"] != null ? row["graph_node_identifier"].ToString() : null,
                 BucketGUID = row["bucket_guid"] != null ? row["bucket_guid"].ToString() : null,
                 ObjectGUID = row["object_guid"] != null ? row["object_guid"].ToString() : null,
                 ObjectKey = row["object_key"] != null ? row["object_key"].ToString() : null,
                 ObjectVersion = row["object_version"] != null ? row["object_version"].ToString() : null,
+                Model = row["model"] != null ? row["model"].ToString() : null,
                 Content = row["content"] != null ? row["content"].ToString() : null,
                 CreatedUtc = row["created_utc"] != null ? Convert.ToDateTime(row["created_utc"].ToString()) : DateTime.UtcNow
             };
