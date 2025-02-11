f38006abf634b7de44fdadbe4bcae0a938349582
Joel Christner <joel.christner@gmail.com>
2025-02-11 08:33:20 -0800

    Type detection URL fix

--- src/Test.TypeDetector/Program.cs
+++ src/Test.TypeDetector/Program.cs
@@ -17,7 +17,7 @@
         private static bool _RunForever = true;
         private static Guid _TenantGUID = default(Guid);
         private static string _AccessKey = "default";
-        private static string _Endpoint = "http://localhost:8000/v1.0/tenants/default/processing/typedetection";
+        private static string _Endpoint = "http://localhost:8000/";
         private static ViewTypeDetectorSdk _Sdk = null;
         private static Serializer _Serializer = new Serializer();
         private static bool _EnableLogging = true;
