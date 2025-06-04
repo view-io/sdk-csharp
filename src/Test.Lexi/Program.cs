d30c3966839b2c51327ae7dfc2fed734e8712fe6
Farhan <farhansl@weblineapps.com>
2025-06-04 14:09:00 +0530

    Reapply "Reapply "Implemented Enumerate, RetrieveTopTerms, Exists Method in Lexi Collectionmethods sdk""
    This reverts commit a2106a1c098477804ba8593a9b87a73234d33434.

--- src/Test.Lexi/Program.cs
+++ src/Test.Lexi/Program.cs
@@ -13,7 +13,8 @@
     {
         private static bool _RunForever = true;
         private static Guid _TenantGuid = default(Guid);
-        private static string _Endpoint = "http://localhost:8000/";
+        private static string _Endpoint = "http://view.homedns.org:8000/";
+        private static string _AccessKey = "default";
         private static ViewLexiSdk _Sdk = null;
         private static Serializer _Serializer = new Serializer();
         private static bool _EnableLogging = true;
@@ -22,7 +23,8 @@
         {
             _TenantGuid =   Inputty.GetGuid("Tenant GUID :", _TenantGuid);
             _Endpoint   = Inputty.GetString("Endpoint    :", _Endpoint, false);
-            _Sdk = new ViewLexiSdk(_TenantGuid, _Endpoint);
+            _AccessKey = Inputty.GetString("Access key  :", _AccessKey, false);
+            _Sdk = new ViewLexiSdk(_TenantGuid, _AccessKey, _Endpoint);
             if (_EnableLogging) _Sdk.Logger = EmitLogMessage;
 
             while (_RunForever)
@@ -60,7 +62,12 @@
                     case "del coll":
                         DeleteCollection().Wait();
                         break;
-
+                    case "enum colls":
+                        EnumerateCollections().Wait();
+                        break;
+                    case "topterms coll":
+                        RetrieveCollectionTopTerms().Wait();
+                        break;
                     case "docs":
                         RetrieveAllDocuments().Wait();
                         break;
@@ -101,6 +108,8 @@
             Console.WriteLine("  coll stats    Retrieve collection statistics");
             Console.WriteLine("  write coll    Create collection");
             Console.WriteLine("  del coll      Delete collection");
+            Console.WriteLine("  enum colls    Enumerate collections");
+            Console.WriteLine("  topterms coll     Retrieve collection top terms");
             Console.WriteLine("");
             Console.WriteLine("  docs          List documents in collection");
             Console.WriteLine("  doc           Retrieve document from collection");
@@ -108,7 +117,7 @@
             Console.WriteLine("  write doc     Write document");
             Console.WriteLine("  del doc       Delete document");
             Console.WriteLine("");
-            Console.WriteLine("  enumerate     Enumerate collection");
+            Console.WriteLine("  enumerate     Enumerate collection documents");
             Console.WriteLine("  search        Search collection");
             Console.WriteLine("");
         }
@@ -249,12 +258,28 @@
             EnumerateResponse(result);
         }
 
+        private static async Task EnumerateCollections()
+        {
+            int maxKeys = Inputty.GetInteger("Max keys :", 5, true, false);
+            EnumerationResult<Collection> result = await _Sdk.Collection.Enumerate(maxKeys);
+            EnumerateResponse(result);
+        }
+
         private static async Task SearchCollection()
         {
             SearchResult result = await _Sdk.Search.Search(
                 GetCollectionGuid(),
                 BuildSearchQuery());
             EnumerateResponse(result);
+        } 
+
+        private static async Task RetrieveCollectionTopTerms()
+        {
+            Guid collectionGuid = GetCollectionGuid();
+            int maxKeys = Inputty.GetInteger("Max top terms :", 10, true, false);
+            
+            CollectionTopTerms topTerms = await _Sdk.Collection.RetrieveTopTerms(collectionGuid, maxKeys);
+            EnumerateResponse(topTerms);
         } 
     }
 }
---
+++ src/View.Sdk/CollectionTopTerms.cs
@@ -0,0 +1,68 @@
+namespace View.Sdk
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+
+    /// <summary>
+    /// Collection top terms.
+    /// </summary>
+    public class CollectionTopTerms
+    {
+        #region Public-Members
+
+        /// <summary>
+        /// Dictionary of terms and their frequencies.
+        /// </summary>
+        [JsonExtensionData]
+        public Dictionary<string, object> Terms { get; set; } = new Dictionary<string, object>();
+        
+        /// <summary>
+        /// Helper method to get term frequency as integer
+        /// </summary>
+        /// <param name="term">The term to get frequency for</param>
+        /// <returns>The frequency as integer, or 0 if not found</returns>
+        public int GetTermFrequency(string term)
+        {
+            if (Terms.TryGetValue(term, out object value))
+            {
+                if (value is JsonElement element)
+                {
+                    return element.ValueKind == System.Text.Json.JsonValueKind.Number ? element.GetInt32() : 0;
+                }
+                else if (value is int intValue)
+                {
+                    return intValue;
+                }
+            }
+            return 0;
+        }
+
+        #endregion
+
+        #region Private-Members
+
+        #endregion
+
+        #region Constructors-and-Factories
+
+        /// <summary>
+        /// Instantiate.
+        /// </summary>
+        public CollectionTopTerms()
+        {
+
+        }
+
+        #endregion
+
+        #region Public-Methods
+
+        #endregion
+
+        #region Private-Methods
+
+        #endregion
+    }
+}
