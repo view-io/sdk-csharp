b80d460b51f4826cb72577e223048dbc3b160981
Farhan <farhansl@weblineapps.com>
2025-10-09 10:20:24 +0530

    Removed unused Using statement

--- src/View.Sdk/ModelEndpoint.cs
+++ src/View.Sdk/ModelEndpoint.cs
@@ -2,8 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Reflection.Emit;
-    using System.Text.Json.Serialization;
 
     /// <summary>
     /// Model endpoint.
